868aecf562526fd5c0b2d6944afc0e21de5d495a
tanmaysinghx <tanmaysinghx@gmail.com>
2023-11-25 23:23:08 +0530

    Creating components

---
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-dashboard.component.html',
+  styleUrl: './admin-dashboard.component.scss'
+})
+export class AdminDashboardComponent {
+
+}
