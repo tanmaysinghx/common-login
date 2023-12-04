1cab380a67e2c24093d60b8b66e803c2d697b9a0
Tanmay Singh <tanmaysinghx@gmail.com>
2023-12-04 18:54:13 +0530

    Project-portfolio screen design complted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { ProjectPortfolioComponent } from './project-portfolio/project-portfolio.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'admin-dashboard', component: AdminDashboardComponent },
+    { path: 'project-portfolio', component: ProjectPortfolioComponent}
 ];
--- src/app/project-portfolio/project-portfolio.component.ts
+++ src/app/project-portfolio/project-portfolio.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-project-portfolio',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './project-portfolio.component.html',
   styleUrl: './project-portfolio.component.scss'
 })
 export class ProjectPortfolioComponent {
 
+
+  applications = [
+    { name: 'Citrix Gateway', description: 'Description for Citrix Gateway' },
+    { name: 'ServiceNow', description: 'Description for ServiceNow' },
+    { name: 'ServiceNow', description: 'Description for ServiceNow' },
+    { name: 'ServiceNow', description: 'Description for ServiceNow' },
+    { name: 'ServiceNow', description: 'Description for ServiceNow' },
+    { name: 'ServiceNow', description: 'Description for ServiceNow' },
+    { name: 'ServiceNow', description: 'Description for ServiceNow' },
+    { name: 'ServiceNow', description: 'Description for ServiceNow' },
+    // Add more application objects here
+  ];
 }
